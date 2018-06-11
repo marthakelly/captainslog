33b19241ac835ec07efc788b83c974bdab92891e
Martha Schumann <martha@uber.com>
2018-06-11 15:46:48 -0700

    initial commit! work is a prototype and is a bit slop sloppy. will clean up in subsequent commits

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# captainslog
+# Captain's Log
+
+## record the audio memo (attempt 1)
+* iOS, get Google Drive app from the app store
+* create a new folder in Google Drive
+* use 'Voice Memos' app on iOS
+* save the memo
+* then click the 'share' icon and copy it to Drive
+
+## SETUP
+* Get a Twilio number
+* Create a new project in the GCP Console https://console.cloud.google.com/
+* Activate free trial to get $300
+* Enable Google Cloud Functions API
+* Enable Cloud Speech API
+* Enable Cloud Natural Language API
+* Create a Cloud Storage bucket https://cloud.google.com/storage/docs/quickstart-console (captainslogstorage)
+* Install Google Cloud SDK https://cloud.google.com/sdk/
+* set up account
+*  set up project name, login, set project ID
+* Install beta components (?)
+* gcloud components install beta
+* Once you have a phone number, click Manage Numbers and then click on your phone number.
+* Manage numbers: https://www.twilio.com/console/phone-numbers/incoming
+* Click 'Voice'
+* Under Voice:
+* Set Configure with to Webhooks/TwiML.
+* Set A Call Comes In to Webhook and enter the following URL:
+* https://us-central1-[YOUR_PROJECT_ID].cloudfunctions.net/handleCall
+* Replace [YOUR_PROJECT_ID] with your Google Cloud Platform project ID.
+* Click Save.
+* return to Twilio account settings https://www.twilio.com/console/account/settings
+* and take note of the Auth Token for your Live Credentials. You will need it later in this tutorial
+* npm init
+* npm install --save twilio got @google-cloud/speech @google-cloud/language @google-cloud/storage
+* Create a file named config.json with the following contents: 
+* {
+*   "RESULTS_BUCKET": "[RESULTS_BUCKET]",
+*   "TWILIO_AUTH_TOKEN": "[YOUR_TWILIO_AUTH_TOKEN]"
+* }
+* gcloud beta functions deploy handleCall --trigger-http --stage-bucket=captainslogstorage
+* deploy handleCall
+* gcloud beta functions deploy handleCall --trigger-http --stage-bucket=[YOUR_STAGE_BUCKET]
+* deploy getRecording
+* gcloud beta functions deploy getRecording --trigger-http --stage-bucket=[YOUR_STAGE_BUCKET]
+* deploy analyzeRecording
+* gcloud beta functions deploy analyzeRecording --trigger-bucket=[RESULTS_BUCKET] --stage-bucket=[YOUR_STAGE_BUCKET] --timeout=240
+
+
+# TODO
+* upgrade twilio
+* custom message when twilio picks up!
---
+++ index.js
@@ -0,0 +1,179 @@
+'use strict';
+
+const config = require('./config.json');
+const twilio = require('twilio');
+
+const VoiceResponse = twilio.twiml.VoiceResponse;
+
+const projectId = process.env.GCLOUD_PROJECT;
+const region = 'us-central1';
+
+function isValidRequest (req, res, pathname) {
+  let isValid = true;
+
+  // Only validate that requests came from Twilio when the function has been
+  // deployed to production.
+  if (process.env.NODE_ENV === 'production') {
+    isValid = twilio.validateExpressRequest(req, config.TWILIO_AUTH_TOKEN, {
+      url: `https://${region}-${projectId}.cloudfunctions.net/${pathname}`
+    });
+  }
+
+  // Halt early if the request was not sent from Twilio
+  if (!isValid) {
+    res
+      .type('text/plain')
+      .status(403)
+      .send('Twilio Request Validation Failed.')
+      .end();
+  }
+
+  return isValid;
+}
+
+exports.handleCall = (req, res) => {
+  if (!isValidRequest(req, res, 'handleCall')) {
+    return;
+  }
+
+  const recordingStatusCallbackUrl = `https://${region}-${projectId}.cloudfunctions.net/getRecording`;
+
+  // Prepare a response to the voice call
+  const response = new VoiceResponse();
+
+  // Prompt the user to leave a message
+  response.say('Hello from Cloud Functions. Please leave a message after the beep.');
+
+  console.log('Recording message.');
+
+  // Record the user's message
+  response.record({
+    // Limit the recording to 60 seconds
+    maxLength: 60,
+    // Give Twilio the deployed url of the other function for when the recorded
+    // audio is available
+    recordingStatusCallback: eecordingStatusCallbackUrl
+  });
+
+  // End the call
+  response.hangup();
+
+  // Send the response
+  res
+    .status(200)
+    .type('text/xml')
+    .send(response.toString())
+    .end();
+};
+
+exports.getRecording = (req, res) => {
+  if (!isValidRequest(req, res, 'getRecording')) {
+    return;
+  }
+
+  const got = require('got');
+  const path = require('path');
+  const storage = require('@google-cloud/storage')();
+
+  const filename = `recordings/${path.parse(req.body.RecordingUrl).name}/audio.wav`;
+  const file = storage
+    .bucket(config.RESULTS_BUCKET)
+    .file(filename);
+
+  console.log(`Saving recording to: ${filename}`);
+
+  got.stream(req.body.RecordingUrl)
+    .pipe(file.createWriteStream({
+      metadata: {
+        contentType: 'audio/x-wav'
+      }
+    }))
+    .on('error', (err) => {
+      console.error(err);
+      res
+        .status(500)
+        .send(err)
+        .end();
+    })
+    .on('finish', () => {
+      res
+        .status(200)
+        .end();
+    });
+};
+
+exports.analyzeRecording = (event) => {
+  const object = event.data;
+
+  if (object.resourceState === 'not_exists') {
+    // Ignore file deletions
+    return true;
+  } else if (!/^recordings\/\S+\/audio\.wav$/.test(object.name)) {
+    // Ignore changes to non-audio files
+    return true;
+  }
+
+  console.log(`Analyzing gs://${object.bucket}/${object.name}`);
+
+  // Import the Google Cloud client libraries
+  const gl = require('@google-cloud/language');
+  const nl = new gl.LanguageServiceClient();
+  const gs = require('@google-cloud/speech');
+  const speech = new gs.SpeechClient();
+  const storage = require('@google-cloud/storage')();
+
+  const bucket = storage.bucket(object.bucket);
+  const dir = require('path').parse(object.name).dir;
+
+  // Configure audio settings for Twilio voice recordings
+  const audioConfig = {
+    sampleRateHertz: 8000,
+    encoding: 'LINEAR16',
+    languageCode: 'en-US'
+  };
+
+  const audio = {
+    // content: bucket.file(object.name)
+    uri: `gs://${object.bucket}/${object.name}`
+  };
+
+  const request = {
+    audio,
+    config: audioConfig
+  };
+
+  // Transcribe the audio file
+  return speech.recognize(request)
+    // Perform Sentiment, Entity, and Syntax analysis on the transcription
+    .then(data => {
+      const results = data[0]
+            .results
+            .map(result => result.alternatives[0].transcript)
+            .join('\n');
+
+      console.log(`Results: ${results}`);
+
+      return nl.annotateText({
+        document: {
+          type: 1,
+          content: results
+        },
+        features: {
+          extractSyntax: true,
+          extractEntities: true,
+          extractDocumentSentiment: true,
+          extractEntitySentiment: true,
+          classifyText: true
+        }
+      })
+    })
+    // Finally, save the analysis
+    .then(data => {
+      const filename = `analyses/analysis+${Date.now()}.json`;
+      console.log(`Saving gs://${object.bucket}/${filename}`);
+
+      return bucket
+        .file(filename)
+        .save(JSON.stringify(data[0], null, 2));
+    });
+};
